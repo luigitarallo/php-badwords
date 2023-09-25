be6edfe159b23fe8f762b1984b1396bfd0356ed2
luigitarallo <tarallo.gigi@gmail.com>
2023-09-25 19:18:29 +0200

    feat: add strlen, str_replace, echo body

--- badwords.php
+++ badwords.php
@@ -1,7 +1,9 @@
 <?php
-
 $paragraph = $_GET['paragraph'];
 $censored= $_GET['badwords'];
+$lenghtParagraph = strlen($paragraph);
+$censoredParagraph= str_replace( $censored,'***', $paragraph);
+$lenghtCensoredParagraph= strlen($censoredParagraph);
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +14,9 @@ $censored= $_GET['badwords'];
     <title>Bad Words</title>
 </head>
 <body>
-    <p>
-        <?php echo $paragraph ?>
-    </p>
+    <p><?php echo $paragraph . ' ';  ?></p>
+    <p><?php echo 'paragraph length ' . $lenghtParagraph . ' chars'; ?></p>
+    <p><?php echo $censoredParagraph . ' '; ?></p>
+    <p><?php echo 'censored paragraph length ' . $lenghtCensoredParagraph . ' chars'; ?></p>
 </body>
 </html>
